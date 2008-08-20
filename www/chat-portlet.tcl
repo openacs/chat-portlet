6038ed98b78aa492d26f4725f9cb9b953f1fda85
Emmanuelle Raffenne <eraffenne@gmail.com>
2008-08-20 17:41:16 +0000

    Added link to enter chat room in html mode

--- www/chat-portlet.tcl
+++ www/chat-portlet.tcl
@@ -45,13 +45,15 @@ if { $community_id eq 0 } {
 } else {
 	set query_name "rooms_list"
 }
-db_multirow -extend { can_see_p room_enter_url } rooms $query_name {} {
+db_multirow -extend { can_see_p room_enter_url room_html_url html_text } rooms $query_name {} {
 	set can_see_p 0
 	if { $user_p || $admin_p } {
 		set can_see_p 1
 		set num_rooms [expr $num_rooms + 1]
 	}   
     set room_enter_url [export_vars -base "${base_url}room-enter" {room_id {client $default_mode}}]
+    set room_html_url [export_vars -base "${base_url}room-enter" {room_id {client html}}]
+    set html_text [_ chat.html_client_msg]
 }
 
 template::list::create -name chat_rooms -multirow rooms \
@@ -65,6 +67,12 @@ template::list::create -name chat_rooms -multirow rooms \
         }
         description {
             label "[_ chat.Description]"
+        }
+        html_mode {
+            label "[_ chat-portlet.html_mode]"
+            link_url_col room_html_url
+            display_col html_text
+            link_html {title "[_ chat.Enter_html_pretty_name]"}
         }
     }
 
