fedb171929c54666485eddad3b0c4fb307f401cf
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-06-20 09:04:30 +0000

    - modernize SQL

--- sql/postgresql/chat-portlet-create.sql
+++ sql/postgresql/chat-portlet-create.sql
@@ -20,15 +20,21 @@
 --   @creation-date 2004-10-10
 --   @version $Id: chat-portlet-create.sql,v 0.1 2004/10/10
 
-create function inline_0()
-returns integer as '
-declare
+
+
+--
+-- procedure inline_0/0
+--
+CREATE OR REPLACE FUNCTION inline_0(
+
+) RETURNS integer AS $$
+DECLARE
   ds_id portal_datasources.datasource_id%TYPE;
-begin
+BEGIN
 
   ds_id = portal_datasource__new(
-    ''chat_portlet'',                                           -- Name
-    ''Chat portlet''                                            -- description     
+    'chat_portlet',                                           -- Name
+    'Chat portlet'                                            -- description     
   );
 
   --  the standard 4 params
@@ -36,47 +42,47 @@ begin
   -- shadeable_p 
   perform portal_datasource__set_def_param (
         ds_id,                                          -- datasource_id
-        ''t'',                                          -- config_required_p
-        ''t'',                                          -- configured_p 
-        ''shadeable_p'',                        -- key 
-        ''t''                                           -- value
+        't',                                          -- config_required_p
+        't',                                          -- configured_p 
+        'shadeable_p',                        -- key 
+        't'                                           -- value
 );      
 
 
   -- hideable_p 
   perform portal_datasource__set_def_param (
         ds_id,
-        ''t'',
-        ''t'',
-        ''hideable_p'',
-        ''t''
+        't',
+        't',
+        'hideable_p',
+        't'
 );      
 
   -- user_editable_p 
   perform portal_datasource__set_def_param (
         ds_id,
-        ''t'',
-        ''t'',
-        ''user_editable_p'',
-        ''f''
+        't',
+        't',
+        'user_editable_p',
+        'f'
 );      
 
   -- shaded_p 
   perform portal_datasource__set_def_param (
         ds_id,
-        ''t'',
-        ''t'',
-        ''shaded_p'',
-        ''f''
+        't',
+        't',
+        'shaded_p',
+        'f'
 );      
 
   -- link_hideable_p 
   perform portal_datasource__set_def_param (
         ds_id,
-        ''t'',
-        ''t'',
-        ''link_hideable_p'',
-        ''t''
+        't',
+        't',
+        'link_hideable_p',
+        't'
 );  
 
 -- chat-specific params
@@ -84,97 +90,98 @@ begin
   -- community_id must be configured
   perform portal_datasource__set_def_param (
         ds_id,
-        ''t'',
-        ''f'',
-        ''package_id'',
-        ''''
+        't',
+        'f',
+        'package_id',
+        ''
 );
 
   return 0;
 
-end;' language 'plpgsql';
+END;
+$$ LANGUAGE plpgsql;
 
 select inline_0();
 
 drop function inline_0();
 
 
-create function inline_1()
-returns integer as '
-begin
+CREATE OR REPLACE FUNCTION inline_1() RETURNS integer AS $$
+BEGIN
 
         -- create the implementation
         perform acs_sc_impl__new (
-                ''portal_datasource'',
-                ''chat_portlet'',
-                ''chat_portlet''
+                'portal_datasource',
+                'chat_portlet',
+                'chat_portlet'
         );
 
         -- add all the hooks
         perform  acs_sc_impl_alias__new (
-               ''portal_datasource'',
-               ''chat_portlet'',
-               ''GetMyName'',
-               ''chat_portlet::get_my_name'',
-               ''TCL''
+               'portal_datasource',
+               'chat_portlet',
+               'GetMyName',
+               'chat_portlet::get_my_name',
+               'TCL'
         );
 
         perform  acs_sc_impl_alias__new (
-               ''portal_datasource'',
-               ''chat_portlet'',
-               ''GetPrettyName'',
-               ''chat_portlet::get_pretty_name'',
-               ''TCL''
+               'portal_datasource',
+               'chat_portlet',
+               'GetPrettyName',
+               'chat_portlet::get_pretty_name',
+               'TCL'
         );
 
         perform  acs_sc_impl_alias__new (
-               ''portal_datasource'',
-               ''chat_portlet'',
-               ''Link'',
-               ''chat_portlet::link'',
-               ''TCL''
+               'portal_datasource',
+               'chat_portlet',
+               'Link',
+               'chat_portlet::link',
+               'TCL'
         );
 
         perform  acs_sc_impl_alias__new (
-               ''portal_datasource'',
-               ''chat_portlet'',
-               ''AddSelfToPage'',
-               ''chat_portlet::add_self_to_page'',
-               ''TCL''
+               'portal_datasource',
+               'chat_portlet',
+               'AddSelfToPage',
+               'chat_portlet::add_self_to_page',
+               'TCL'
         );
 
         perform  acs_sc_impl_alias__new (
-               ''portal_datasource'',
-               ''chat_portlet'',
-               ''Show'',
-               ''chat_portlet::show'',
-               ''TCL''
+               'portal_datasource',
+               'chat_portlet',
+               'Show',
+               'chat_portlet::show',
+               'TCL'
         );
 
         perform  acs_sc_impl_alias__new (
-               ''portal_datasource'',
-               ''chat_portlet'',
-               ''Edit'',
-               ''chat_portlet::edit'',
-               ''TCL''
+               'portal_datasource',
+               'chat_portlet',
+               'Edit',
+               'chat_portlet::edit',
+               'TCL'
         );
 
         perform  acs_sc_impl_alias__new (
-               ''portal_datasource'',
-               ''chat_portlet'',
-               ''RemoveSelfFromPage'',
-               ''chat_portlet::remove_self_from_page'',
-               ''TCL''
+               'portal_datasource',
+               'chat_portlet',
+               'RemoveSelfFromPage',
+               'chat_portlet::remove_self_from_page',
+               'TCL'
         );
 
         -- Add the binding
         perform acs_sc_binding__new (
-            ''portal_datasource'',
-            ''chat_portlet''
+            'portal_datasource',
+            'chat_portlet'
         );
 
         return 0;
-end;' language 'plpgsql';
+END;
+$$ LANGUAGE plpgsql;
 
 select inline_1();
 
