f3fa0a5baaf152ead6f7f778411b345247de19a4
Hector Romojaro <hector.romojaro@gmail.com>
2018-10-01 15:19:24 +0000

    Add editor hints

--- www/chat-portlet.tcl
+++ www/chat-portlet.tcl
@@ -76,3 +76,9 @@ template::list::create -name chat_rooms -multirow rooms \
             label "[_ chat.Description]"
         }
     }
+
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 4
+#    indent-tabs-mode: nil
+# End:
