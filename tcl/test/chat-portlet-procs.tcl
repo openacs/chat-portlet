c157ae3ad37918462726c47c5b1ca77564fed4c9
Hector Romojaro <hector.romojaro@gmail.com>
2020-08-21 17:33:27 +0000

    Add 'chat_admin_portlet::get_pretty_name' and 'chat_portlet::get_pretty_name' to 'chat_portlet_links_names' test case

--- tcl/test/chat-portlet-procs.tcl
+++ tcl/test/chat-portlet-procs.tcl
@@ -9,14 +9,18 @@ ad_library {
 aa_register_case -procs {
         chat_admin_portlet::link
         chat_portlet::link
+        chat_admin_portlet::get_pretty_name
+        chat_portlet::get_pretty_name
     } -cats {
         api
         production_safe
-    } chat_portlet_links {
-        Test diverse link procs.
+    } chat_portlet_links_names {
+        Test diverse link and name procs.
 } {
-    aa_equals "Chat admin portlet link" "[chat_admin_portlet::link]" ""
-    aa_equals "Chat portlet link"       "[chat_portlet::link]" ""
+    aa_equals "Chat admin portlet link"         "[chat_admin_portlet::link]" ""
+    aa_equals "Chat portlet link"               "[chat_portlet::link]" ""
+    aa_equals "Chat portlet pretty name"        "[chat_portlet::get_pretty_name]" "#chat-portlet.pretty_name#"
+    aa_equals "Chat portlet admin pretty name"  "[chat_admin_portlet::get_pretty_name]" "#chat-portlet.admin_pretty_name#"
 }
 
 # Local variables:
